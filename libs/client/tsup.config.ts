07569347cd29b7590a9187c4ee92299657cdc056
Hyun Kim <hkim@live.com.au>
2026-04-06 23:22:17 +0100

    netlify

--- libs/client/tsup.config.ts
+++ libs/client/tsup.config.ts
@@ -47,7 +47,7 @@ export default url;
 export default defineConfig({
   entry: ["src/index.ts"],
   format: ["cjs", "esm"],
-  dts: !process.env.CI,
+  dts: true,
   clean: true,
   sourcemap: true,
   loader: {
